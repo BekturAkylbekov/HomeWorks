dade94f8c136ababadf932d937ca93eaa65dc854
BekturAkylbekov <144769054+BekturAkylbekov@users.noreply.github.com>
2024-10-14 16:11:10 +0600

    firstcommit

---
+++ src/main/java/Triangle.java
@@ -0,0 +1,9 @@
+public class Triangle {
+    double a,b,c;
+    public void area(){
+        double p = (a + b + c) / 2;
+
+        double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        System.out.println(area);
+    }
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+
+
+        Scanner scanner =new Scanner(System.in);
+        Triangle triangle =new Triangle();
+        System.out.print("Введите длину первой стороны: ");
+        triangle.a= scanner.nextDouble();
+        System.out.print("Введите длину второй стороны: ");
+        triangle.b= scanner.nextDouble();
+        System.out.print("Введите длину третьей стороны: ");
+        triangle.c= scanner.nextDouble();
+        triangle.area();
+    }
+}
