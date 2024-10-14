47e253f8eb604ae2a2eb4246f2df5ad351b66e6b
BekturAkylbekov <144769054+BekturAkylbekov@users.noreply.github.com>
2024-10-14 17:44:41 +0600

    коммин делал

--- src/main/java/Triangle.java
+++
@@ -1,9 +0,0 @@
-public class Triangle {
-    double a,b,c;
-    public void area(){
-        double p = (a + b + c) / 2;
-
-        double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-        System.out.println("Площадь треугольника: " +area);
-    }
-}
